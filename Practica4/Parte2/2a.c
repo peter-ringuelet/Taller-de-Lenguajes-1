5856cac7edf857c98dfe33ffc5dc3b98f509536e
peter.ringuelet@gmail.com <peter.ringuelet@gmail.com>
2023-05-10 10:52:21 -0300

    termine ejercicio 2

--- Practica4/Parte2/2a.c
+++ Practica4/Parte2/2a.c
@@ -9,12 +9,15 @@ double promedio(double *, int);
 int main(){
     double *arreglo = NULL;
     int n;
+    double prom;
     printf("Ingrese la dimension de la matriz: ");
     scanf("%d", &n);
     reservar_memoria(&arreglo, n);
     cargar_arreglo(arreglo, n);
     imprimir_arreglo(arreglo, n);
     free(arreglo);
+    prom = promedio(arreglo,n);
+    printf("%f", prom);
     return 0;
 }
 
@@ -24,20 +27,23 @@ void reservar_memoria(double **arreglo, int n){
 
 void cargar_arreglo(double *arreglo, int n){
     double numero;
-    for (int i = 0; i < n; arreglo++, i++){
+    for (int i = 0; i < n; i++){
         printf("Ingrese un numero: ");
         scanf("%lf", &numero);
-        *arreglo = numero;
+        arreglo[i] = numero;
     }
 }
 
 void imprimir_arreglo(double * arreglo, int n){
-    for (int i =0; i< n; arreglo++, i++){
-        printf("Numero (%d): %f \n", (i+1),*arreglo);
+    for (int i =0; i< n; i++){
+        printf("Numero (%d): %f \n", (i+1),arreglo[i]);
     }
 }
 
 double promedio(double *arreglo, int n){
     double tot = 0;
+    for (int i =0; i< n; i++){
+        tot+= arreglo[i];
+    }
+    return tot/n;
 }
-
---
+++ Practica4/Parte2/2b.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void reservar_memoria(double **, int);
+void cargar_arreglo(double *, int);
+void imprimir_arreglo(double *, int);
+double promedio(double *, int);
+
+int main(){
+    double *arreglo = NULL;
+    int n;
+    double prom;
+    printf("Ingrese la dimension de la matriz: ");
+    scanf("%d", &n);
+    reservar_memoria(&arreglo, n);
+    cargar_arreglo(arreglo, n);
+    imprimir_arreglo(arreglo, n);
+    free(arreglo);
+    prom = promedio(arreglo,n);
+    printf("%f", prom);
+    return 0;
+}
+
+void reservar_memoria(double **arreglo, int n){
+    *arreglo = (double *)malloc(n*sizeof(double));
+}
+
+void cargar_arreglo(double *arreglo, int n){
+    double numero;
+    for (int i = 0; i < n; arreglo++, i++){
+        printf("Ingrese un numero: ");
+        scanf("%lf", &numero);
+        *arreglo = numero;
+    }
+}
+
+void imprimir_arreglo(double * arreglo, int n){
+    for (int i =0; i< n; arreglo++, i++){
+        printf("Numero (%d): %f \n", (i+1),*arreglo);
+    }
+}
+
+double promedio(double *arreglo, int n){
+    double tot = 0;
+    for (int i =0; i< n; arreglo++, i++){
+        tot+= *arreglo;
+    }
+    return tot/n;
+}
+
