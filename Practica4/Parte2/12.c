36dfb8e990a18c0e8d336795439b334469fd1415
peter.ringuelet@gmail.com <peter.ringuelet@gmail.com>
2023-05-12 09:08:11 -0300

    Matriz como arreglo

---
+++ Practica4/Parte2/12.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void reservar_memoria(int **, int, int);
+void cargar_matriz(int *, int, int);
+void imprimir_matriz(int *, int, int);
+double promedio(int *, int, int);
+void liberar_matriz(int **matriz, int n, int m);
+
+int main(){
+    int *matriz = NULL;
+    int n, m;
+    double prom;
+    printf("Ingrese la cantidd de filas: ");
+    scanf("%d", &n);
+    printf("Ingrese la cantidd de columnas: ");
+    scanf("%d", &m);
+    reservar_memoria(&matriz, n, m);
+    cargar_matriz(matriz, n, m);
+    imprimir_matriz(matriz, n, m);
+    prom = promedio(matriz, n, m);
+    printf("%f", prom);
+    liberar_matriz(&matriz, n, m);
+    return 0;
+}
+
+void reservar_memoria(int **matriz, int n, int m){
+    *matriz = (int *)malloc(n*m*sizeof(int));
+}
+
+void cargar_matriz(int *matriz, int n, int m){
+    int numero;
+    for (int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            printf("Ingrese un numero (%d,%d): ", i,j);
+            scanf("%d", &numero);
+            matriz[ i * m + j] = numero;
+        }
+    }
+}
+
+void imprimir_matriz(int * matriz, int n, int m){
+    for (int i =0; i< n; i++){
+        for(int j = 0; j < m; j++){
+            printf("%d  ", matriz[ i * m + j]);
+        }
+        printf("\n");
+    }
+}
+
+double promedio(int *matriz, int n, int m){
+    double tot = 0;
+    for (int i =0; i< n; i++){
+        for (int j=0; j<m; j++){
+            tot+= matriz[ i * m + j];
+        }
+    }
+    return tot/(n*m);
+}
+
+void liberar_matriz(int **matriz, int n, int m){
+    free(*matriz);
+    *matriz = NULL;
+}
