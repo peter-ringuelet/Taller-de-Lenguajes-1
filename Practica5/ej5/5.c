78cd985f74ee95ad577d3787b3e088a95e2b7c98
Pedro Ringuelet <peter.ringuelet@gmail.com>
2023-05-24 10:19:36 -0300

    Lo entendi mal

--- Practica5/ej5/5.c
+++ Practica5/ej5/5.c
@@ -1,108 +1,131 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-struct nodo {
+struct nodo
+{
     char dato[100];
-    struct nodo * sig;
+    struct nodo *sig;
 };
 
-void inicializar(struct nodo**);
-void agregarInicio(struct nodo **, char []);
-void agregarFinal(struct nodo **, int);
-int cantElementos(struct nodo * lista);
-void imprimirElementos(struct nodo * lista);
-void eliminarElementos(struct nodo ** lista);
-void cargarLista(struct nodo ** lista);
+void inicializar(struct nodo **);
+void apilar(struct nodo **, char[]);
+void imprimirElementos(struct nodo *lista);
+void liberarMemoria(struct nodo **lista);
+void cargarLista(struct nodo **lista);
+char *desapilar(struct nodo **lista);
 
-
-int main(){
-    struct nodo * lista;
+int main()
+{
+    struct nodo *lista;
+    char palabra[100];
+    char *dicc;
+    int encontrado;
+    FILE *f;
+    f = fopen("diccionario.txt", "r");
+    if (f == NULL)
+    {
+        printf("\nError al abrir archivo fuente\n");
+        return 1;
+    }
     inicializar(&lista);
     cargarLista(&lista);
     printf("Lista 1: \n");
     imprimirElementos(lista);
-    elimiarElemento(&lista, 1);
-    printf("Lista Modificada: \n");
-    imprimirElementos(lista);
 
-    eliminarElementos(&lista);
+    while (lista != NULL)
+    {
+        encontrado = 0;
+        dicc = desapilar(&lista);
+        fseek(f, 0, SEEK_SET); // Restart file reading
+        while (fscanf(f, "%s", palabra) == 1)
+        {
+            if (strcmp(palabra, dicc) == 0)
+            {
+                printf("%s se encuentra en el diccionario\n", dicc);
+                encontrado = 1;
+                break;
+            }
+        }
+        if (encontrado == 0)
+        {
+            printf("%s no se encuentra en el diccionario\n", dicc);
+        }
+        free(dicc);
+    }
+
+    fclose(f);
+
+    liberarMemoria(&lista);
     imprimirElementos(lista);
 
     return 0;
 }
 
-void inicializar( struct nodo ** lista){
+void inicializar(struct nodo **lista)
+{
     *lista = NULL;
 }
 
-void agregarInicio(struct nodo ** lista, char dato[]){
-    struct nodo * nuevo = (struct nodo*) malloc(sizeof(struct nodo));
-    nuevo->dato = dato;
+void apilar(struct nodo **lista, char dato[])
+{
+    struct nodo *nuevo = (struct nodo *)malloc(sizeof(struct nodo));
+    strcpy(nuevo->dato, dato);
     nuevo->sig = *lista;
     *lista = nuevo;
 }
 
-void agregarFinal(struct nodo ** lista, int dato){
-    struct nodo * nuevo = (struct nodo *) malloc(sizeof(struct nodo));
-    struct nodo * aux = *lista;
-    if (*lista == NULL){
-        agregarInicio(lista,dato);
-    }
-    else{
-        while((aux)->sig != NULL){
-            aux = aux-> sig;
-        }
-        nuevo->dato = dato;
-        nuevo->sig = NULL;
-        aux->sig = nuevo;
-    }
-}
-
-int cantElementos(struct nodo * lista){
-    int cant = 0;
+void imprimirElementos(struct nodo *lista)
+{
     struct nodo *aux = lista;
-    while(aux != NULL){
-        aux = aux->sig;
-        cant++;
-    }
-    return cant;
-}
-
-void imprimirElementos(struct nodo * lista){
-    struct nodo * aux = lista;
-    while(aux != NULL){
-        printf("%d \n", aux->dato);
+    while (aux != NULL)
+    {
+        printf("%s \n", aux->dato);
         aux = aux->sig;
     }
 }
 
-void eliminarElementos(struct nodo ** lista) {
+void liberarMemoria(struct nodo **lista)
+{
     struct nodo *aux;
 
-    while (*lista != NULL) {
+    while (*lista != NULL)
+    {
         aux = *lista;
         *lista = (*lista)->sig;
         free(aux);
     }
 }
 
-struct nodo * invertirLista(struct nodo * lista){
-    struct nodo * invertida;
-    inicializar(&invertida);
-    while(lista != NULL){
-        agregarInicio(&invertida, lista->dato);
-        lista = lista->sig;
-    }
-    return invertida;
-}
-
-void cargarLista(struct nodo ** lista){
+void cargarLista(struct nodo **lista)
+{
     char string[100];
     printf("Ingrese una palabra: ");
     scanf("%s", string);
-    while(string != "ZZZ"){
-        agregarInicio(lista,string);
+    while (strcmp(string, "ZZZ"))
+    {
+        apilar(lista, string);
         printf("Ingrese una palabra: ");
         scanf("%s", string);
     }
+}
+
+char *desapilar(struct nodo **lista)
+{
+    if (*lista == NULL)
+        return NULL; // return null if list is empty
+
+    struct nodo *aux;
+    aux = *lista;
+
+    char *dicc = malloc(sizeof(char) * 100); // allocate memory for dicc
+    if (dicc == NULL)
+        return NULL; // return null if memory allocation failed
+
+    strcpy(dicc, aux->dato);
+
+    *lista = aux->sig; // update the start of the list
+    free(aux);         // free the memory of the popped node
+
+    return dicc;
 }
