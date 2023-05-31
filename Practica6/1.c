aedcb38a3b05a75d2445f408d7143035f0ea63ee
peter.ringuelet@gmail.com <peter.ringuelet@gmail.com>
2023-05-31 10:39:22 -0300

    Que imprime, ej boludo

---
+++ Practica6/1.c
@@ -0,0 +1,11 @@
+#include <stdio.h> 
+#include <stdlib.h>
+int main (int argc, char * argv[]) { 
+    printf("\nargc = %d",argc); 
+    printf("\nargv[0] => %s",argv[0]); 
+    return 0;
+}
+
+//Imprime:
+//argc = 1
+//argv[0] => /Users/peter/Desktop/Facu/Taller 1/Practica6/1
