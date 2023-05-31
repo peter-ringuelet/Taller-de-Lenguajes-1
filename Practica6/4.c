3c0678a8057a4359dc134fdc882c4adf346f29dd
peter.ringuelet@gmail.com <peter.ringuelet@gmail.com>
2023-05-31 11:50:54 -0300

    macro x^2

---
+++ Practica6/4.c
@@ -0,0 +1,11 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <time.h>
+#define SQUAREOF(x) (x)*(x)     // Antes era x * x y esta MAL
+int main() { 
+    int xin=3;
+    printf("\nxin=%i",xin); 
+    printf("\nSQUAREOF(xin)=%i", SQUAREOF(xin));
+    printf("\nSQUAREOF(xin+4)=%i", SQUAREOF(xin+4)); 
+    printf("\nSQUAREOF(xin+xin)=%i", SQUAREOF(xin+xin));
+}
